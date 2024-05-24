b7c0cbec87a2e392d0ce0783ea18d665200c0264
Philip Zingmark <philip.zingmark@gmail.com>
2024-05-24 11:26:03 +0200

    optional chain fix? + formatting

--- src/components/Header/Pages.ts
+++ src/components/Header/Pages.ts
@@ -1,25 +1,25 @@
 export type Page = {
-    path: string;
-    label: string;
-    isMenu: boolean
-    roles?: string[];
+  path: string;
+  label: string;
+  isMenu: boolean;
+  roles?: string[];
 };
 
 export const pages: Page[] = [
-    {
-        path: "/",
-        label: "Hem",
-        isMenu: true,
-    },
-    {
-        path: "/dashboard/ansvarig",
-        label: "Dashboard",
-        isMenu: true,
-        roles: ["phöz", "ansvarig"]
-    },
-    {
-        path: "/reg-points",
-        label: "Registrera poäng",
-        isMenu: true,
-    },
+  {
+    path: "/",
+    label: "Hem",
+    isMenu: true,
+  },
+  {
+    path: "/dashboard/ansvarig",
+    label: "Dashboard",
+    isMenu: true,
+    roles: ["phöz", "ansvarig"],
+  },
+  {
+    path: "/reg-points",
+    label: "Registrera poäng",
+    isMenu: true,
+  },
 ];
